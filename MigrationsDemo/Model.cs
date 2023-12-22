22a80a17ac8c86b03a04f9abe4b03ef8aca092c9
AndrewKorchemniy <andrewkorch@gmail.com>
2023-12-21 18:27:13 -0800

    Add project files.

---
+++ MigrationsDemo/Model.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MigrationsDemo;
+
+class BlogContext : DbContext
+{
+    public DbSet<Post> Posts { get; set;}
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+}
+
+class Post
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+}
---
+++ MigrationsDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using MigrationsDemo;
+
+using var db = new BlogContext();
+
+if (!db.Posts.Any())
+{
+    Console.WriteLine("Adding sample data...");
+    db.AddRange(
+        new Post { Title = "Intro to Migrations" },
+        new Post { Title = "Migrations in Team Environments" },
+        new Post { Title = "Advanced Migrations"});
+    db.SaveChanges();
+}
+
+Console.WriteLine("Posts:");
+foreach (var post in db.Posts)
+{
+    Console.WriteLine($"    {post.Title}");
+}
