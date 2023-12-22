a0b246bec03db397e3dee88f5fa924c755fba96e
AndrewKorchemniy <andrewkorch@gmail.com>
2023-12-21 18:33:23 -0800

    add post author migration

--- MigrationsDemo/Migrations/20231222022938_AddPostAuthor.Designer.cs
+++ MigrationsDemo/Migrations/20231222022938_AddPostAuthor.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MigrationsDemo;
 
@@ -10,9 +11,11 @@ using MigrationsDemo;
 namespace MigrationsDemo.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20231222022938_AddPostAuthor")]
+    partial class AddPostAuthor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
